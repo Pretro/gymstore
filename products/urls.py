3b6b9959dc0f4a10f6e1226e88dbc5d896cd1d6f
Pretro <jort2212@outlook.com>
2021-05-02 10:58:48 +0000

    Created the shopping cart template

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-    path('category/<category_id>', views.filter_products_by_category, name='product_category'),
-    
+    path('category/<category_id>', views.filter_products_by_category, name='product_category'),  # noqa:501
 ]
