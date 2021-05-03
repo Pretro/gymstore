20469273ae0e13f9c39bfc34dd109cc93c360cb2
Pretro <jort2212@outlook.com>
2021-05-03 14:05:20 +0000

    Fixed some bugs problems in the shopping cart

--- gymstore/settings.py
+++ gymstore/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'products.context_processors.menu_links',
+                'products.context_processors.counter',
             ],
         },
     },
