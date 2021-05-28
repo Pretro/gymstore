b7a85ef0811fc2fa062a3c0d3c7d4db0aaafddad
Pretro <jort2212@outlook.com>
2021-05-28 12:25:44 +0000

    Created editing product section for store owners

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<product_id>', views.product_detail, name='product_detail'),
     path('category/<category_id>', views.filter_products_by_category, name='product_category'),  # noqa:501
     path('add/', views.add_product, name='add_product'),
+     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
