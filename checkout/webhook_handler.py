f369baed998713bcc2bba30328330591da4da7c3
Pretro <jort2212@outlook.com>
2021-05-28 19:56:52 +0000

    Creted delete product for the Product admin section

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -23,17 +23,19 @@ class StripeWH_Handler:
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
+        print(order)
 
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        print(body)
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-           )
+        )
+        print('send_mail', cust_email)
 
     def handle_event(self, event):
         """
--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<product_id>', views.product_detail, name='product_detail'),
     path('category/<category_id>', views.filter_products_by_category, name='product_category'),  # noqa:501
     path('add/', views.add_product, name='add_product'),
-     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),  # noqa:501
 ]
