cdd1c46de1afabdb1fcb0f84ce0108d944ee455e
Pretro <jort2212@outlook.com>
2021-05-29 20:16:55 +0000

    Removed secret key and set debug

--- gymstore/settings.py
+++ gymstore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vk+nclib7b)m1r%_rht@93ur%($*q#m$3)l!2#vxydhnrsq=wr'  # noqa:501
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gymstore.herokuapp.com', 'localhost']
 
